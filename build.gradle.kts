ed08f9f3bd6726da20a46ef637c1e80993197a32
jnngl <jnngles@gmail.com>
2024-07-24 20:26:09 +1100

    Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    java
+    id("io.papermc.paperweight.userdev").version("1.7.1")
+    id("xyz.jpenilla.run-paper").version("2.3.0")
+    id("io.github.goooler.shadow").version("8.1.7")
+}
+
+group = "com.jnngl"
+version = "1.0.1"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "papermc-repo"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
+        name = "sonatype"
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+}
+
+dependencies {
+    paperweightDevelopmentBundle("io.papermc.paper:dev-bundle:1.21-R0.1-SNAPSHOT")
+    implementation("net.elytrium:serializer:1.1.1")
+    implementation("com.jnngl:mapcolor:1.0.1")
+    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    implementation("com.j256.ormlite:ormlite-jdbc:6.1")
+    implementation("org.xerial:sqlite-jdbc:3.45.0.0")
+    implementation("org.bstats:bstats-bukkit:3.0.2")
+}
+
+java {
+    val version = JavaLanguageVersion.of(21)
+    toolchain.languageVersion.set(version)
+}
+
+tasks {
+    shadowJar {
+        relocate("net.elytrium.serializer", "com.jnngl.vanillaminimaps.serializer")
+        relocate("org.bstats", "com.jnngl.vanillaminimaps.metrics")
+        exclude("org/slf4j/**")
+        minimize()
+    }
+
+    processResources {
+        val props = mapOf("version" to version)
+        inputs.properties(props)
+        filteringCharset = "UTF-8"
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+
+    reobfJar {
+        dependsOn(shadowJar)
+    }
+}
